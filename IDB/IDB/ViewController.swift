04f684532bc4471db4d9d046c00a24d3dd8341f1
Daeyun Ethan Kim <nicebuvr@naver.com>
2017-11-04 17:21:46 +0900

    [프로젝트 생성]

---
+++ IDB/IDB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IDB
+//
+//  Created by Daeyun Ethan Kim on 04/11/2017.
+//  Copyright © 2017 Daeyun Ethan Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
